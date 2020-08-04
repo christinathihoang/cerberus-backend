a3c0e5f070ce0a9cf0a2c364cf5077a2b7c4f3d4
christinathihoang <christinahoang@utexas.edu>
2020-08-03 23:05:29 -0500

    create migrations and seeds for users and applications

---
+++ knex/migrations/applications.ts
@@ -0,0 +1,18 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('applications', (table: Knex.TableBuilder) => {
+    table.increments();
+    table.string('nickname').notNullable();
+    table.string('meaning').notNullable();
+    table.string('pronunciation').notNullable();
+    table.boolean('approved').notNullable();
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('applications');
+}
+
---
+++ knex/migrations/users.ts
@@ -0,0 +1,18 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('users', (table: Knex.TableBuilder) => {
+        table.increments();
+        table.string('first_name').notNullable();
+        table.string('last_name').notNullable();
+        table.string('email').notNullable();
+        table.string('password').notNullable();
+      })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('users')
+}
+
