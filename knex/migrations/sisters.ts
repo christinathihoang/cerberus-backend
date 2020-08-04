9cf859de27238e1f9488b6ed4451a6416cb6686c
christinathihoang <christinahoang@utexas.edu>
2020-08-03 22:30:28 -0500

    set up knexfile, migration, and seed

---
+++ knex/migrations/sisters.ts
@@ -0,0 +1,16 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('sisters', (table: Knex.TableBuilder) => {
+    table.increments();
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.string('tree').notNullable();
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('sisters');
+}
