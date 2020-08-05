bd9de6c633badafdb22ba10268b5f9f43caecb50
christinathihoang <christinahoang@utexas.edu>
2020-08-05 00:47:55 -0500

    seed sisters

--- knex/migrations/sisters.ts
+++ knex/migrations/sisters.ts
@@ -6,7 +6,9 @@ export async function up(knex: Knex): Promise<void> {
     table.increments();
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
+    table.string('nickname').notNullable();
     table.string('tree').notNullable();
+    table.string('chapter').notNullable();
   })
 }
 
