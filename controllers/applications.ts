99b0568f8e4b392d40376a7a258dd1fc82edde04
christinathihoang <christinahoang@utexas.edu>
2020-08-08 12:57:10 -0500

    get all applications with little information included

--- controllers/applications.ts
+++ controllers/applications.ts
@@ -3,14 +3,42 @@ import { knex } from "../app";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  knex("applications").then((applications: any) => {
-    res.status(200).json(applications);
-  });
+  knex("applications")
+    .then((applications: any) => {
+      return Promise.all(applications.map((application: any) => {
+        return knex("sisters")
+          .where({ id: application.little_id })
+          .then((little) => {
+            application.little = little;
+            return application;
+          });
+      }));
+    })
+    .then((response: any) => {
+      console.log(response);
+      res.status(200).json(response);
+    });
 });
 
 router.get("/:id", (req, res) => {
   knex("applications")
     .where({ id: req.params.id })
+    .then((application: any) => {
+      knex("sisters")
+        .where({ id: application.little_id })
+        .then((little) => {
+          application.little = little;
+          res.status(200).json(little);
+        });
+    })
+    .catch((err: any) => {
+      res.json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  knex("applications")
+    .insert(req.body)
     .then((application: any) => {
       res.status(200).json(application);
     })
@@ -18,16 +46,5 @@ router.get("/:id", (req, res) => {
       res.json(err);
     });
 });
-
-router.post('/', (req, res) => {
-  knex('applications')
-  .insert(req.body)
-  .then((application: any) => {
-    res.status(200).json(application)
-  })
-  .catch((err: any) => {
-    res.json(err)
-  })
-})
 
 export default router;
