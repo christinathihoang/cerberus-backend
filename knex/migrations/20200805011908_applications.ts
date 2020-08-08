2c249357345dfc3cc890fb482f8db5dd2525c9c6
christinathihoang <christinahoang@utexas.edu>
2020-08-08 12:31:28 -0500

    update application schema with sister_id

--- knex/migrations/20200805011908_applications.ts
+++ knex/migrations/20200805011908_applications.ts
@@ -4,6 +4,7 @@ import * as Knex from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('applications', (table: Knex.TableBuilder) => {
     table.increments();
+    table.integer('sister_id').notNullable();
     table.string('nickname').notNullable();
     table.string('meaning').notNullable();
     table.string('pronunciation').notNullable();
